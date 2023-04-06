667f40cc225199ad520df2e484e916627c09df52
ArtemKataev <tipa.atlet@mail.ru>
2023-04-06 21:09:34 +0300

    meeting of player in c#

---
+++ console4_Lovelypolsovatel/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("enter username");
+string username =Console.ReadLine();
+
+if (username.ToLower() == "artem")
+{
+  Console.WriteLine("hayo broooo");
+}
+else
+{
+  Console.WriteLine("hello bastard");
+  Console.WriteLine(username);
+}
